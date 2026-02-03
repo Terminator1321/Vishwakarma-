3a57795bcd0eb16c9e2c19c715407176ee20bf00
Rohan das <rohandas1321@gmail.com>
2026-02-03 23:44:37 +0530

    made pins and links in more structured manner

--- Panel/Node/Node.cpp
+++ Panel/Node/Node.cpp
@@ -3,20 +3,39 @@
 #pragma region Node Implementation
 
 // Node implementation
-Node::Node(std::string node, ImVec2 size) : node_name(node), size(size) {}
+Node::Node(std::string node, ImVec2 size) : node_name(node), size(size)
+{
+    for (int i = 0; i < inputpins; ++i)
+    {
+        ImVec2 pin_pos(0.0f, (size.y / (inputpins + 1)) * (i + 1));
+        input_data.pins.emplace_back(Pins(pin_pos, current_draw_list));
+        input_data.pin_positions.push_back(pin_pos);
+        input_data.pin_names.push_back("In " + std::to_string(i + 1)); // we will change it in future with the real name of input parameters
+    }
+    for (int i = 0; i < outputpins; ++i)
+    {
+        ImVec2 pin_pos(size.x, (size.y / (outputpins + 1)) * (i + 1));
+        output_data.pins.emplace_back(Pins(pin_pos, current_draw_list));
+        output_data.pin_positions.push_back(pin_pos);
+        output_data.pin_names.push_back("Out " + std::to_string(i + 1)); // we will change it in future with the real name of return types
+    }
+}
 
 void Node::SpawnNode(ImDrawList *draw_list, ImVec2 canvas_origin, ImVec2 local_pos, ImVec2 pan_offset)
 {
+    current_draw_list = draw_list;
     ImVec2 p1(canvas_origin.x + local_pos.x + pan_offset.x, canvas_origin.y + local_pos.y + pan_offset.y);
     ImVec2 text_size = ImGui::CalcTextSize(node_name.c_str());
 
     float width = std::max(size.x, text_size.x + padding * 2.0f);
     float height = std::max(size.y, text_size.y + padding * 2.0f);
+
     ImVec2 out_l(p1.x + width + 1.0f, p1.y + height * 0.5f);
     ImVec2 p2(p1.x + width, p1.y + height);
+
     ImGui::SetCursorScreenPos(p1);
     ImGui::InvisibleButton(node_name.c_str(), ImVec2(width, height));
-    out_link = new link(out_l);
+
     active = ImGui::IsItemActive();
     if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left) && active)
     {
@@ -26,29 +45,31 @@ void Node::SpawnNode(ImDrawList *draw_list, ImVec2 canvas_origin, ImVec2 local_p
     {
         selected = false;
     }
-    draw_list->AddRectFilled(p1, p2, (isSPECIALNODE ? theme_color.red() : theme_color.blue()), 6.0f);
+    current_draw_list->AddRectFilled(p1, p2, (isSPECIALNODE ? theme_color.red() : theme_color.blue()), 6.0f);
     if (selected)
     {
-        draw_list->AddRect(p1, p2, (isSPECIALNODE ? theme_color.green() : theme_color.yellow()), 6.0f, 0, 8.0f);
+        current_draw_list->AddRect(p1, p2, (isSPECIALNODE ? theme_color.green() : theme_color.yellow()), 6.0f, 0, 8.0f);
     }
 
-    draw_list->AddText(ImVec2(p1.x + padding, p1.y + padding), theme_color.white(), node_name.c_str());
-    InputsAOutputsPinHolders(draw_list, out_l, p1, theme_color, isSPECIALNODE, out_link);
-}
-
-void Node::InputsAOutputsPinHolders(ImDrawList *draw_list, ImVec2 out, ImVec2 in, color theme_color, bool isSpecial, link *outlink)
-{
-    ImGui::SetCursorScreenPos(out);
-    ImGui::InvisibleButton("##", ImVec2(6.0f, 6.0f));
-    draw_list->AddCircleFilled(out, 6.0f, theme_color.white());
-    if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
+    current_draw_list->AddText(ImVec2(p1.x + padding, p1.y + padding), theme_color.white(), node_name.c_str());
+    for (int i = 0; i < inputpins; ++i)
     {
-        out_link->DrawLink(draw_list,ImGui::GetMousePos());
+        ImVec2 pin_pos(p1.x, p1.y + (height / (inputpins + 1)) * (i + 1));
+        input_data.pins[i].DrawPin(current_draw_list, pin_pos, ImVec2(0, 0));
+        input_data.pin_positions[i] = pin_pos;
+    }
+    for (int i = 0; i < outputpins; ++i)
+    {
+        ImVec2 pin_pos(p1.x + width, p1.y + (height / (outputpins + 1)) * (i + 1));
+        output_data.pins[i].DrawPin(current_draw_list, pin_pos, ImVec2(0, 0));
+        output_data.pin_positions[i] = pin_pos;
     }
 }
 
 #pragma endregion
 
+#pragma region Link Implementation
+// Link implementation
 link::link(ImVec2 pos) : position(pos) {}
 void link::DrawLink(ImDrawList *draw_list, ImVec2 p1)
 {
@@ -63,3 +84,43 @@ bool link::IsPinHovered(ImVec2 pin_pos, float radius)
 {
     return false;
 }
+
+#pragma endregion
+
+#pragma region Pins Implementation
+// Pins implementation
+Pins::Pins(ImVec2 pos, ImDrawList *draw_list) : position(pos), current_draw_list(draw_list)
+{
+    _link = new link(position);
+};
+
+void Pins::DrawPin(ImDrawList *draw_list, ImVec2 p1, ImVec2 p2)
+{
+    UpdatePosition(p1);
+    _link->UpdatePosition(position);
+    draw_list->AddCircleFilled(position, 5.0f, theme_color.white()); // hard coded radius for now
+}
+
+bool Pins::IsPinHovered(ImVec2 pin_pos, float radius)
+{
+    ImVec2 mos_pos = ImGui::GetIO().MousePos;
+    float dx = mos_pos.x - pin_pos.x;
+    float dy = mos_pos.y - pin_pos.y;
+    return (dx * dx + dy * dy) <= (radius * radius);
+}
+
+void Pins::OnMouseDragBeginOverEvent()
+{
+    if (!IsPinHovered(position, 5.0f))
+        return; // hard coded radius for now
+    if (ImGui::IsMouseDragging(ImGuiMouseButton_Left))
+    {
+        _link->DrawLink(current_draw_list, ImGui::GetIO().MousePos);
+    }
+    if(ImGui::IsMouseReleased(ImGuiMouseButton_Left) && !_link->isConnected())
+    {
+        // Here we will check if we are releasing over another pin to create a connection
+    }
+}
+
+#pragma endregion
--- Panel/Node/Node.hpp
+++ Panel/Node/Node.hpp
@@ -1,23 +1,51 @@
 #ifndef NODE_HPP
 #define NODE_HPP
-#include"imgui.h"
-#include"../GUIThemeHelper/theme.hpp"
+#include "imgui.h"
+#include "../GUIThemeHelper/theme.hpp"
 #include <string>
 #include <cstdint>
 #include <cmath>
-class link{
+#include <vector>
+class link
+{
+private:
+    color theme_color;
+    ImVec2 position;
+    bool connected = false;
+public:
+    link(ImVec2 pos);
+    void DrawLink(ImDrawList *draw_list, ImVec2 p1);
+    bool IsPinHovered(ImVec2 pin_pos, float radius);
+    bool isConnected() const { return connected; }
+    void setConnected(bool conn) { connected = conn; }
+    void UpdatePosition(ImVec2 pos) { position = pos; }
+};
+
+class Pins
+{
     private:
         color theme_color;
         ImVec2 position;
-    
+        ImDrawList *current_draw_list;
+        link *_link;
     public:
-        link(ImVec2 pos);
-        void DrawLink(ImDrawList *draw_list, ImVec2 p1);
+        Pins(ImVec2 pos, ImDrawList *draw_list);
+        void UpdatePosition(ImVec2 pos) { position = pos; }
+        void DrawPin(ImDrawList *draw_list, ImVec2 p1,ImVec2 p2);
         bool IsPinHovered(ImVec2 pin_pos, float radius);
+        void OnMouseDragBeginOverEvent();
 };
 
-class Node {
+class Node
+{
     private:
+        struct NodeData{
+            // std::vector<ImU32> colors; for future use
+            // std::vector<std::string> datatypes; for future use
+            std::vector<Pins> pins;
+            std::vector<ImVec2> pin_positions;
+            std::vector<std::string> pin_names;
+        };
         std::string node_name = "default node";
         ImVec2 size = ImVec2(100.0f, 50.0f);
         float padding = 10.0f;
@@ -26,17 +54,21 @@ class Node {
         color theme_color;
         bool isSPECIALNODE = false;
         int32_t id;
-        link *out_link;
+        int inputpins = 1;
+        int outputpins = 1;
+        NodeData input_data;
+        NodeData output_data;
+        ImDrawList *current_draw_list;
     public:
         Node(std::string node, ImVec2 size);
-        void SpawnNode(ImDrawList* draw_list, ImVec2 canvas_origin, ImVec2 local_pos, ImVec2 pan_offset);
+        void SpawnNode(ImDrawList *draw_list, ImVec2 canvas_origin, ImVec2 local_pos, ImVec2 pan_offset);
         bool isSelected() const { return selected; }
         void setSelected(bool sel) { selected = sel; }
         std::string getName() const { return node_name; }
         bool isActive() const { return active; }
         void setSPECIALNODE(bool special) { isSPECIALNODE = special; }
         bool isSPECIAL() const { return isSPECIALNODE; }
-        void InputsAOutputsPinHolders(ImDrawList *draw_list, ImVec2 out,ImVec2 in, color theme_color, bool isSpecial, link *outlink);
+        
 };
 
 #endif
